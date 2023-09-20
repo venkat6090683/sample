df8331e4ee0ff12ff76cf7e966b0607e726277a0
venkatasaikumar <venkat6090683@gmail.com>
2023-09-20 11:30:56 +0530

    second commit

--- sample/src/sample_package/sample_class.java
+++ sample/src/sample_package/sample_class.java
@@ -3,6 +3,7 @@ package sample_package;
 public class sample_class {
 	public static void main(String[] args) {
 		System.out.println("sample class");
+		System.out.println("sample calsee2");
 	}
 
 }
